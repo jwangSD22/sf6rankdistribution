b7544fe7b22f460fc18935d29683b8fa1e01d498
unknown <jackw1689@gmail.com>
2023-09-28 21:55:07 -0700

    sf6 scraper and bargraph

--- src/main.py
+++ src/main.py
@@ -1,2 +1,38 @@
-def test():
-    pass
+
+import pandas as pd
+import time
+from scraper import scrape
+
+leagues = ['Rookie','Iron','Bronze','Silver','Gold','Platinum','Diamond','Master']
+
+league = []
+division = []
+players = []
+
+
+for item in leagues:
+
+    if item != 'Master':
+        for _ in range(1,6):
+            league.append(item)
+            division.append(_)
+
+league.append('Master')
+division.append(1)
+
+for index in range(36):
+    time.sleep(10)
+    player_count = scrape(league[index],division[index],index+1)
+    players.append(player_count)
+    print(f'Finished {league[index]} {division[index]} with {player_count} players.')
+
+
+time.sleep(3)
+
+dict = {'League':league,'Division':division,'Number of Players':players}
+
+df = pd.DataFrame(dict)
+
+print(df)
+
+df.to_csv('rank_distribution.csv')
---
+++ src/scraper.py
@@ -0,0 +1,44 @@
+from selenium import webdriver 
+from selenium.webdriver.chrome.service import Service as ChromeService 
+from webdriver_manager.chrome import ChromeDriverManager 
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+def scrape(league,league_division,rank_page):
+    
+    url = f'https://www.streetfighter.com/6/buckler/ranking/league?character_filter=1&character_id=luke&platform=1&user_status=1&home_filter=2&home_category_id=5&home_id=1&league_rank={rank_page}&page=1' 
+    driver = webdriver.Chrome(service=ChromeService( 
+        ChromeDriverManager().install())) 
+
+    driver.get(url) 
+    driver.add_cookie({
+        "name":"buckler_praise_date","value":"1695945638704"
+    })
+    driver.add_cookie({
+        "name":"buckler_r_id","value":"ffacf239-a620-4795-aa40-944eb2c851ab"
+    })
+    driver.add_cookie({
+        "name":"pll_language","value":"en"
+    })
+    driver.add_cookie({
+        "name":"buckler_id","value":"33RUGpVfr3B_1pmws3z4dtJ0TQ--UK8GWj-E5EtcFl2SL0TAqhLvdvxczzKxUaoS"
+    })
+
+    # Reload the page to apply the cookies
+    driver.refresh()
+    # Wait for the page to load completely (you can customize the timeout)
+    ranking_info = driver.find_element(By.CLASS_NAME, "ranking_ranking_now__last__TghLM")
+
+
+    print(ranking_info.text)
+
+    split = ranking_info.text.split(' ')
+
+    
+
+
+    driver.quit()
+
+    return split[1]
+  
